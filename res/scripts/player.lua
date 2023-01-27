f3624136c89062e883a5b1e24b3834cb4ee0777a
szczypiorofix <szczypiorofix@o2.pl>
2023-01-28 00:46:12 +0100

    Added new resources and scripts

---
+++ res/scripts/player.lua
@@ -0,0 +1,10 @@
+local player = {}
+
+player.x = 10
+player.y = 12
+
+function player.print()
+    print("Player.print() call." )
+end
+
+return player
---
+++ res/scripts/script1.lua
@@ -0,0 +1,17 @@
+player = require("res/scripts/player")
+
+print("Hello from Lua! Lua version: ", _VERSION )
+
+x = 47
+
+player.print()
+
+function GetSomeValue()
+    return 24
+end
+
+
+function DawajMnieTupla()
+    return "dupa", "dupa2"
+end
+
